e37562570a083940361717aeb2d4125e2c757275
Sarif Hossain <hossainsarif007@gmail.com>
2026-03-15 13:30:17 +0900

    added new file

---
+++ script/login.js
@@ -0,0 +1,18 @@
+// get the button with id
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById('account-number').value;
+    const password = document.getElementById('pin').value;
+    const convertedPassword = parseInt(password);
+    if (accountNumber.length === 13){
+        if(convertedPassword == 12345678){
+            window.location.href = "./main.html"
+        }
+        else{
+            alert('invalid password');
+        }
+    }
+    else{
+        alert('invalid id');
+    }
+})
